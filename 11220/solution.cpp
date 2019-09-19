286c9b74d02865ce0b3975bb3fddc20a210e50fc
daven3302 <daven3302@gmail.com>
2019-09-19 19:56:24 +0800

    Create solution.cpp

---
+++ 11220/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+    int num;
+    string cache;
+    vector<vector<string>>data;
+    while (cin >> num)
+    {
+        getline(cin,cache);
+        for(int i = 0 ; i < num ; i++)
+        {
+            getline(cin,cache);
+            if(cache == "")
+                continue;
+            
+        }
+    }
+    
+}
