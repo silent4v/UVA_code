70a2d21ec512eb4eaac8bbd8c48e0e124895065a
daven3302 <daven3302@gmail.com>
2019-09-21 18:49:18 +0800

    2019/9/21 18:49

--- 10221/solution.cpp
+++ 10221/solution.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #include <cmath>
-#define MIN  60
-#define pi 3.1415926535
+#include <iomanip>
+#define MIN 60
 using namespace std;
-double chord(int x , int y , string flag)
+double chord(int x, int y, string flag)
 {
     double cache = (double)y;
-    if(flag == "min")
+    if (flag == "min")
         cache /= 60;
-    cache = cache * pi / 180;
+    cache = cache * acos(-1) / 180;
     double ans = 2 * x * x * (1 - cos(cache));
     ans = sqrt(ans);
     return ans;
 }
-double arc(int x , int y , string flag)
+double arc(int x, int y, string flag)
 {
     double cache = (double)y;
-    if(flag == "min")
+    if (flag == "min")
         cache /= 60;
-    cache = cache * pi / 180;
+    cache = cache * acos(-1) / 180;
     double ans = x * cache;
     return ans;
 }
 
 int main()
 {
-    int distance , angle;
+    int distance, angle;
     string unit;
-    while(cin >> distance >> angle >> unit)
+    //cout << fixed << setprecision(100) << acos(-1);
+    while (cin >> distance >> angle >> unit)
     {
         distance += 6440;
-        printf("%.6lf\n",arc(distance,angle,unit));
-        printf("%.6lf\n",chord(distance,angle,unit));
+        printf("%.6lf\n", arc(distance, angle, unit));
+        printf("%.6lf\n", chord(distance, angle, unit));
     }
     return 0;
 }
